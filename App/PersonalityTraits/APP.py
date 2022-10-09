1fc3d057b52015ad6d697758c8addfff13576c5e
MADDUMAGE T.A.R <it19160580@my.sliit.lk>
2022-10-09 12:25:19 +0530

    Update APP.py

--- App/PersonalityTraits/APP.py
+++ App/PersonalityTraits/APP.py
@@ -1,4 +1,6 @@
-from flask import Flask, request, jsonify 
+import datetime
+
+from flask import Flask, request, jsonify
 import flask
 from flask_cors import CORS, cross_origin
 import re
@@ -22,11 +24,16 @@ app.config["DEBUG"] = True
 
 @app.route('/personalitytraits', methods=['GET'])
 def personalitytraits():
+    print("-------------------------------------------------------------------")
     data = request.get_json(force=True)
     video_sas_url = data['video_sas_url']
     r = requests.get(video_sas_url, allow_redirects=True)
     filename = re.search('(?<=research/).*(?=\?)', video_sas_url).group(0).split("/")[1]
+    print("\ndownload the Video from Database \t filename {}".format(filename))
     open(filename, 'wb').write(r.content)
+
+    print("\nStart time: {} \t filename: {}".format(datetime.now(), filename))
+    print("\nVideo Split to the the frames \t filename {}".format(filename))
 
     SplitFrames.FrameCapture(filename)
 
@@ -44,35 +51,16 @@ def personalitytraits():
     model.add(Dropout(0.5))
     model.add(Dense(5, activation='softmax'))
 
-
+    print("\nLoad The Model \t filename {}".format(filename))
     # loading the trained weights
     model.load_weights("weight.hdf5")
 
     # compiling the model
     model.compile(loss='categorical_crossentropy',optimizer='Adam',metrics=['accuracy'])
 
-    # count = 0
-    # # videoFile = test_videos[i]
-    # cap = cv2.VideoCapture('UCF/Hello/Hello_1.mp4')   # capturing the video from the given path
-    # frameRate = cap.get(5) #frame rate
-    # print(frameRate)
-    # x=1
-    # # removing all other files from the temp folder
-    # files = glob('temp/*')
-    # for f in files:
-    #     os.remove(f)
-    # while(cap.isOpened()):
-    #     frameId = cap.get(1) #current frame number
-    #     ret, frame = cap.read()
-    #     if (ret != True):
-    #         break
-    #     if (frameId % math.floor(frameRate) == 0):
-    #         # storing the frames of this particular video in temp folder
-    #         filename ='temp/' + "_frame%d.jpg" % count;count+=1
-    #         cv2.imwrite(filename, frame)
-    # cap.release()
-
+   
     # reading all the frames from temp folder
+    print("\nRead All the frames \t filename {}".format(filename))
     images = glob("Demo_Frames/*.jpg")
 
     prediction_images = []
@@ -86,16 +74,21 @@ def personalitytraits():
     predict = []
     print(len(prediction_images))
     # converting all the frames for a test video into numpy array
+    print("\nconverting all the frames of video into numpy array \t filename {}".format(filename))
     prediction_images = np.array(prediction_images)
     # extracting features using pre-trained model
+    print("\nextracting features using pre-trained model \t filename {}".format(filename))
     prediction_images = base_model.predict(prediction_images)
 
     # converting features in one dimensional array
+    print("\nconverting features in one dimensional array \t filename {}".format(filename))
     print(prediction_images.shape)
     prediction_images = prediction_images.reshape(prediction_images.shape[0], 7*7*512)
     # predicting tags for each array
+    print("\npredicting tags for each array \t filename {}".format(filename))
     prediction = model.predict(prediction_images)
     # appending the mode of predictions in predict list to assign the tag to the video
+    print("\nappending the mode of predictions in predict list to assign the tag to the video \t filename {}".format(filename))
     print(s.mode(prediction))
     print("Prediction mode")
     print(s.mode(prediction)[0][0])
@@ -103,6 +96,7 @@ def personalitytraits():
     print(np.argmax(s.mode(prediction)[0][0]))
     # predict.append(y.columns.values[s.mode(prediction)[0][0]])
     # appending the actual tag of the video
+    print("\nappending the actual tag of the video \t filename {}".format(filename))
     # actual.append(videoFile.split('/')[1].split('_')[1])
     yVals = ["extraversion","neuroticism","agreeableness","conscientiousness","openness"]
     ret = yVals[np.argmax(s.mode(prediction)[0][0])]
@@ -113,7 +107,8 @@ def personalitytraits():
         os.remove(filename)
     except:
         pass
-
+    print("\nProcess End Time", datetime.now.strftime("%H:%M:%S") + "\t filename {}".format(filename))
+    print("\n--------------------------------------------------------------------------")
     return {"personalitytraits": prdict}
 
 app.run(port=5005)
